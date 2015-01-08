f4c73add41951b908eb5521e2055e8d42aa2fe19
Philip Chase <pbc@ufl.edu>
2015-01-08 08:53:38 -0500

    Add read of data file, subsetting of dates and a basic histogram

--- plot1.R
+++ plot1.R
@@ -13,3 +13,18 @@ if (!file.exists(dataFile)){
 
 # Extract the contents of the zip file
 unzip(dataFile)
+
+# define column classes for the data to be read
+colClasses <- c("character", "character", "character", "character", "character", "character", "character", "character", "numeric")
+
+# load the data into a dataframe
+dataFile <- c("household_power_consumption.txt")
+data <- read.table(dataFile, header=TRUE, sep=";", colClasses=colClasses)
+
+# convert the Date column to a POSIXlt date
+data$Date <- strptime(data$Date, format="%d/%m/%Y")
+
+# Subset the data to the two dates of interest
+myData <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02", drop=TRUE)
+
+hist(as.numeric(myData$Global_active_power))
